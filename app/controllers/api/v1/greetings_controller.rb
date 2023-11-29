9d2072dc7c2fd0d85255b127fe4b5c19b0c3174d
Tanmayi Manku <tanmayitanum@gmail.com>
2023-11-29 11:04:47 +0530

    Fix rubocop linters

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
-    def random_greeting
-        @greeting = Greeting.order('RANDOM()').first
-        render json: { greeting: @greeting.message }
-    end
+  def random_greeting
+    @greeting = Greeting.order('RANDOM()').first
+    render json: { greeting: @greeting.message }
+  end
 end
